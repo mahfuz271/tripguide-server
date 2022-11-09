687f86d25914911461812987badded081c475ab6
dev-mahfuz <mrsohag04@gmail.com>
2022-11-09 13:25:41 +0600

    added get reviews by id route

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/reviews/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { service_id: ObjectId(id) }
+            const cursor = ratingCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+        
         //get services
         app.get('/services', async (req, res) => {
             const query = {}
